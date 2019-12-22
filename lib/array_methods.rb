57502d101cdc74733f96bbd36d6eb2c2fc7e96c3
Olivia Madden <madden.livi@gmail.com>
2019-12-22 06:20:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,29 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0 
+  while counter < array.length do
+    if array[counter] == value_to_find
+      return counter
+    else counter +=1
+    end
+  end
+  return nil 
 end
 
 def find_max_value(array)
-  # Add your solution here
+  counter = 0 
+  current_value = array[0]
+  max_value = array[0]
+  while counter < array.length do
+    counter += 1 
+    if array[counter] > current_value
+      
+    end
+  end
+  return max_value
 end
 
+
+
 def find_min_value(array)
-  # Add your solution here
+  counter = 0
 end
